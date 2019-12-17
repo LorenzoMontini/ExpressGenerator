91a6b98661d9dfe1ea9268d0a50a1ac3dc9e56c1
LorenzoMontini <lore252000@gmail.com>
2019-12-17 15:23:43 +0000

    terzo commit

--- routes/user.js
+++ routes/user.js
@@ -12,19 +12,41 @@ router.get('/new', function(req, res, next) {
 router.get('/utenti', function(req,res,next){
     res.render('utenti', {
     title: 'Users',
-    p: people //Passa il vettore alla pagina utenti.pug
+    p: people
+  });
+});
+
+router.get('/poeti/person', function(req,res,next){
+    const person = people.find((m) => m.name === req.query.name);;
+    res.render('poeti', {
+    title: person.name,
+    pt: person
+  });
+});
+
+
+router.get('/info/person', function(req,res,next){
+    const information = people.find((m) => m.name === req.query.name);;
+    res.render('info', {
+    title: information.name,
+    ipt: information
   });
 });
 
 function createFakePerson()
 {
+ people.json=new Object;
  let person;
  let vett = new Array();
+ let texts = new Array();
  var i;
  for (i = 0; i < 10; i++) {
   let randomName = faker.name.findName(); // random name
   let randomEmail = faker.internet.email(); // random mail
-  let randomText = faker.lorem.text(); // random text
+  for (n = 0; n < 5; n++) {
+   let randomText = faker.lorem.text(); // random text
+   texts.push(randomText)
+  }
   let randomCountry = faker.address.country() // random country
   let randomWebSite = faker.internet.url() // random website
   let randomProfile = faker.image.avatar() //random people image
@@ -32,7 +54,7 @@ function createFakePerson()
   person = {
    name:randomName,
    email:randomEmail,
-   text: randomText,
+   text:texts,
    nazionality: randomCountry,
    website: randomWebSite,
    image: randomProfile
